871931759b27b302db282d09a0d338ab08078b9e
Mihail Croitor <mcroitor@gmail.com>
2023-04-21 12:20:38 +0300

    oops

--- generator.php
+++ generator.php
@@ -7,11 +7,11 @@ $short_options = ""; /// "d:u::p::";
 function usage()
 {
     echo "usage: php " . str_replace(__DIR__, "", __FILE__)
-        . " --dsn=<DSN> [--user=<USER> --password=<PASSWORD>] [--output=<OUTPUT_DIR>]";
-    echo "\t\t dsn - connection string";
-    echo "\t\t user - db user, used for MySQL, for example";
-    echo "\t\t password - password for db user";
-    echo "\t\t output - path to outpot, implicitly `./output/`";
+        . " --dsn=<DSN> [--user=<USER> --password=<PASSWORD>] [--output=<OUTPUT_DIR>]" . PHP_EOL;
+    echo "\t\t dsn - connection string" . PHP_EOL;
+    echo "\t\t user - db user, used for MySQL, for example" . PHP_EOL;
+    echo "\t\t password - password for db user" . PHP_EOL;
+    echo "\t\t output - path to outpot, implicitly `./output/`" . PHP_EOL;
 }
 
 $long_options = [
